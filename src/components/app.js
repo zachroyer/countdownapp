09325521587521e83baf4b25a7b41f55d80b6244
Zach Royer <zach.royer14@gmail.com>
2018-07-31 08:16:36 -0600

    installed moment.js, react-datepicker, and placed picker comonent with datepicker within it

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Picker from './picker';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Countdown</h1>
+      <div className='grid'>
+        <h1 className='grid__title'>Birthday Countdown</h1>
+        <p className = 'grid__subtitle'>You can also countdown from whatever you please</p>
+      
+        <Picker/>
       </div>
     );
   }
