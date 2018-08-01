51e6a722ec78ea2e0fc5771b15d20a012889aa51
Zach Royer <zach.royer14@gmail.com>
2018-08-01 08:50:14 -0600

    added changeDate component and inserted icon

---
+++ src/components/changeDate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChangeDate = (title, callback) => {
+    return (
+        <button className='change-date' onClick={callback}>
+            <i className="far fa-calendar"></i>
+            {title}
+        </button>
+    )
+}
+
+export default ChangeDate;
