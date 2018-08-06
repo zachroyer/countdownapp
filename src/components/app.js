9c80e1e10066fe6c59120346922debbc09af748e
Zach <zach.royer14@gmail.com>
2018-08-06 09:33:19 -0600

    large text label and keys

--- src/components/app.js
+++ src/components/app.js
@@ -98,22 +98,31 @@ export default class App extends Component {
         clearInterval(this.timer);
         // document.getElementById("demo").innerHTML = "EXPIRED";
     }
-}.bind(this), 1000);
+  }.bind(this), 1000);
+}.bind(this)
+
+  getBirthDate = function(date) {
+    const month = date.getMonth() + 1; 
+    const day = date.getDate();
+    if(month < 10) {
+      return `0${month}/0${day}`
+    }
+    return `${month}/${day}`
   }.bind(this)
 
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock timeRemaining={this.state.timeRemaining}/>,
+        <Clock key={0}timeRemaining={this.state.timeRemaining}/>,
         ChangeDate('Change Date', () => this.setState({ active: false })),
-        LargeText('04/03'),
-        <label className="grid__remaining">
+        LargeText(this.getBirthDate(this.state.startDate.toDate())),
+        <label key={3}className="grid__remaining">
           Remaining until you turn {this.state.age}
         </label>
       ]
     } else {
       return [
-        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
+        <Picker key={0}startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
         Button('Generate Countdown', () => this.handleGenerate())
       ]
     }
