96a4bbbdb3b4898af5cb7708d3da25b63d6e9a8b
Zach Royer <zach.royer14@gmail.com>
2018-08-06 08:51:33 -0600

    fixed all cases for birthday and currentday calculation

--- src/components/app.js
+++ src/components/app.js
@@ -39,26 +39,40 @@ export default class App extends Component {
 
   handleGenerate = function() {
     this.setState({ active: true })
-      // Set the date we're counting down to
-    
-    var countDownDate = this.state.startDate.toDate().getTime();
 
-    // Update the count down every 1 second
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
+
+    if(birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      }
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      }
+    }
+    
+    var countDownDate = bday.getTime();
+
     this.timer = setInterval(function() {
 
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
+    var now = today.getTime();
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
     const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       const timeRemaining = {
         days,
@@ -69,7 +83,6 @@ export default class App extends Component {
       this.setState({ timeRemaining })
 
 
-    // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(this.timer);
         // document.getElementById("demo").innerHTML = "EXPIRED";
